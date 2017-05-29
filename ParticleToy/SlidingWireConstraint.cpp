8742c592b0154370f755aca5d6341f9453cc10b3
aredev <appierasool@gmail.com>
2017-05-29 18:37:20 +0200

    Constraints working, start with cloth

---
+++ ParticleToy/SlidingWireConstraint.cpp
@@ -0,0 +1,5 @@
+//
+// Created by abdullah on 29/05/2017.
+//
+
+#include "SlidingWireConstraint.h"
---
+++ ParticleToy/SlidingWireConstraint.h
@@ -0,0 +1,14 @@
+//
+// Created by abdullah on 29/05/2017.
+//
+
+#ifndef SIMULATION_1_SLIDINGWIRECONSTRAINT_H
+#define SIMULATION_1_SLIDINGWIRECONSTRAINT_H
+
+
+class SlidingWireConstraint {
+
+};
+
+
+#endif //SIMULATION_1_SLIDINGWIRECONSTRAINT_H
