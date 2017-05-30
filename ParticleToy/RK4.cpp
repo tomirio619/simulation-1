65599aff3b13514a5a0e43fd5fadb1938ecce632
Tom Sandmann <tomirio619@gmail.com>
2017-05-30 16:50:54 +0200

    Changed order of operations

--- ParticleToy/RK4.cpp
+++ ParticleToy/RK4.cpp
@@ -36,42 +36,43 @@ void applyForces(std::vector<Force *> forces){
         std::vector<Vec2f> k4s;
         // Counter
         unsigned i = 0;
+
         // Calculate k1's
         for (auto &particle: particles) {
             particle->m_Velocity += particle->force * dt;
             k1s.push_back(particle->force * dt);
+            particle->m_Position += orgPositions[i] + particle->m_Velocity * dt / 2.0f;
+            i++;
         }
-
+        i = 0;
         // Clear and accumulate forces, apply constraints
         Force::clearForces(particles);
         // Apply forces
         applyForces(forces);
-
         // constraints
         LambdaSolver::solve(particles, constraints, 60, 5);
 
         // Calculate k2's
         for (auto &particle: particles) {
-            particle->m_Position += orgPositions[i] + particle->m_Velocity * dt / 2.0f;
+
             particle->m_Velocity += particle->force * dt;
             k2s.push_back(particle->force + k1s[i] / 2.0f);
+            particle->m_Position += orgPositions[i] + particle->m_Velocity * dt / 2.0f;
             i++;
         }
-
         // Clear and accumulate forces, apply constraints
         i = 0;
         Force::clearForces(particles);
         // Apply forces
         applyForces(forces);
-
         // constraints
         LambdaSolver::solve(particles, constraints, 60, 5);
 
         // Calculate k3's
         for (auto &particle: particles) {
-            particle->m_Position += orgPositions[i] + particle->m_Velocity * dt / 2.0f;
-            particle->m_Velocity += dt / 2.0f;
+            particle->m_Velocity += particle->force * dt;
             k3s.push_back(particle->force + k2s[i] / 2.0f);
+            particle->m_Position = orgPositions[i] + particle->m_Velocity * dt;
             i++;
         }
 
@@ -80,15 +81,12 @@ void applyForces(std::vector<Force *> forces){
         Force::clearForces(particles);
         // Apply forces
         applyForces(forces);
-
-
         // constraints
         LambdaSolver::solve(particles, constraints, 60, 5);
 
         // Calculate k4's and do the final evaluation using the original position
         for (auto &particle: particles) {
-            particle->m_Position += orgPositions[i] + particle->m_Velocity * dt;
-            particle->m_Velocity += dt / 2.0f;
+            particle->m_Velocity += particle->force * dt;
             k4s.push_back(particle->force + k3s[i]);
             particle->m_Position = orgPositions[i] + k1s[i] / 6.0f + k2s[i] / 3.0f + k3s[i] / 3.0f + k4s[i] / 6.0f;
             i++;
